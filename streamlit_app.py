331cd88b1a144e5e52543e14649abd866a3a8ce7
ZeroGPT <125110991+zeroGPTcc@users.noreply.github.com>
2023-05-09 10:03:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome to ZeroGPT.CC
+# How Does the AI ZeroGPT.cc Work?
+Our AI content detector, ZeroGPT.cc, uses massive amounts of data from different sources to precisely predict the origin of a text or a phrase. It has been tested and trained to use combinations of machine learning algorithms alongside natural language processing techniques to present the most accurate results. These algorithms are designed and developed by the expert and professional team of ZeroGPT. The accuracy of these algorithms is backed by several in-house experiments and published highly reputable papers.
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+Our AI text detector works effectively for all versions of GPT models, including GPT-4. You can easily detect whether your text is human-written or AI/GPT Generated. Our AI text detector accurately displays the percentage of AI/GPT plagiarized text for an in-depth analysis of your content.
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+Check out website for more details: https://zerogpt.cc
 
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
